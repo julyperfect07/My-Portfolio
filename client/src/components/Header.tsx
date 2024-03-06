cde4455b49d0b183122b9e9de1e466dc47e270bf
julyperfect07 <abdallah.july05@gmail.com>
2024-03-06 15:50:24 +0300

    Complete the header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=' max-w-7xl py-6 px-3 flex justify-between items-center mx-auto'>
+      <h1 className=' font-bold text-xl'>Abdallah.dev</h1>
+      <div className=' flex gap-3 items-center font-bold'>
+        <h1>Home</h1>
+        <h1>About</h1>
+        <h1>Projects</h1>
+        <h1>Contact</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
