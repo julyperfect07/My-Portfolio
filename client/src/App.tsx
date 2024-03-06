13f6b6eff9c560671d9e98ea9cae0f011bc7ff3a
julyperfect07 <abdallah.july05@gmail.com>
2024-03-06 18:39:25 +0300

    Completing hero section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 function App() {
 
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   )
 }
