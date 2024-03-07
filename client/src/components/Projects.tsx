d7369ee2141dbc4b1e62eb29974d7f53b0c3ee75
julyperfect07 <abdallah.july05@gmail.com>
2024-03-07 22:07:05 +0300

    add another project

--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -5,8 +5,8 @@ const Projects = () => {
     <div className=' max-w-5xl mx-auto mt-52'>
       <h2 className=' uppercase mb-2 font-bold text-blue-600 text-lg'>Portfolio</h2>
       <h1 className='font-bold text-2xl'>Each project is a unique piece of development</h1>
-      <div className=' flex gap-10 max-w-4xl mx-auto mt-24'>
-        <img className=' flex-1 object-cover h-[350px] w-[450px] rounded-sm' src="../../web1.PNG" alt="" />
+      <div className=' flex flex-col md:flex-row gap-10 max-w-4xl mx-auto mt-24'>
+        <img className=' flex-1 object-cover h-[400px] w-full md:w-[450px] rounded-sm' src="../../web1.PNG" alt="" />
         <div className=' flex flex-col items-center text-center flex-1'>
           <h1 className=' uppercase font-bold text-xl'>
             Real Estate Website
@@ -22,8 +22,30 @@ const Projects = () => {
             <span>Redux </span>
           </div>
           <div className=' flex gap-5 font-bold text-xl mt-8 '>
-            <a className='hover:underline hover:text-blue-500' href="https://github.com/julyperfect07/mern-estate">Code</a>
-            <a className='hover:underline hover:text-blue-500' href="https://mern-estate-kmsn.onrender.com/">Live Demo</a>
+            <a className='hover:underline hover:text-blue-500' target='_blank' href="https://github.com/julyperfect07/mern-estate">Code</a>
+            <a className='hover:underline hover:text-blue-500' target='_blank' href="https://mern-estate-kmsn.onrender.com/">Live Demo</a>
+          </div>
+        </div>
+      </div>
+      <div className=' flex flex-col md:flex-row gap-10 max-w-4xl mx-auto mt-24'>
+        <img className=' flex-1 object-cover h-[400px] w-full md:w-[450px] rounded-sm' src="../../web2.PNG" alt="" />
+        <div className=' flex flex-col items-center text-center flex-1'>
+          <h1 className=' uppercase font-bold text-xl'>
+            Blog Website
+          </h1>
+          <p className=' mt-8 text-lg text-center leading-8 text-gray-500'>
+            Modern real estate hub. Upload, filter,and search seamlessly. Enjoy personalized accounts with login and sign out. Crafted with the latest full-stack tech, it's not just a website—it's your key to elegant and innovative home exploration.
+          </p>
+          <div className=' flex gap-5 font-bold text-xl mt-8'>
+            <span>React</span>
+            <span>Nodejs</span>
+            <span>Express</span>
+            <span>Mongodb</span>
+            <span>Redux </span>
+          </div>
+          <div className=' flex gap-5 font-bold text-xl mt-8 '>
+            <a className='hover:underline hover:text-blue-500' target='_blank' href="https://github.com/julyperfect07/mern-blog">Code</a>
+            <a className='hover:underline hover:text-blue-500' target='_blank' href="https://mern-blog-scdo.onrender.com/">Live Demo</a>
           </div>
         </div>
       </div>
